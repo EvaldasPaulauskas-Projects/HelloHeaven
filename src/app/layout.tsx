e775e51289335f96af34ee26b8088afd6ef59570
FinalActt <evaldaspaulauskas2@gmail.com>
2024-03-11 14:36:44 -0700

    Added Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
+import icon from "../../public/icons/favicon-32x32.png"
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/icons/favicon-32x32.png" />
+      </head>
       <body>
       <Navbar />
       {children}
